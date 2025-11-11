7d02c2907cbb9c7325783ef22c5beadb937a1ba4
Devon <d.coleman654@gmail.com>
2025-11-11 14:30:07 -0500

    temp

--- piechenotte/charge_target.gd
+++ piechenotte/charge_target.gd
@@ -1,8 +1,16 @@
-extends Marker2D
+extends Sprite2D
+
+var sourcePos: Vector2
+
+func init(pos: Vector2):
+	sourcePos = pos
 
 func _input(event):
-    if event is InputEventMouseMotion:
-        position = event.position
+	
+	print("input")
+	
+	if event is InputEventMouseMotion:
+		global_position = event.position.clamp(sourcePos, sourcePos + Vector2(100, 100))
 
-    if Input.is_action_just_released("click"):
-        queue_free()
+	if Input.is_action_just_released("click"):
+		queue_free()
--- piechenotte/piece.gd
+++ piechenotte/piece.gd
@@ -3,6 +3,9 @@ extends RigidBody2D
 @export var target_scene: PackedScene
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-    if event.is_action_pressed("click"):
-        var target = target_scene.instantiate()
-        add_child(target)
+	if event.is_action_pressed("click"):
+		var target = target_scene.instantiate()
+		
+		target.init(position)
+		
+		add_child(target)
