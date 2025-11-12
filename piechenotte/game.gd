f5a24146e2326ab177805f69f13b8e01cd454e73
Devon <d.coleman654@gmail.com>
2025-11-12 07:40:34 -0500

    made walls thicker

--- piechenotte/game.gd
+++ piechenotte/game.gd
@@ -5,24 +5,24 @@ const Piece: PackedScene = preload("res://piece.tscn")
 @onready var window: Window = get_window()
 
 func _ready():
-    var shooter = Piece.instantiate()
-    shooter.init(window.size / 2)
+	var shooter = Piece.instantiate()
+	shooter.init(window.size / 2)
 
-    add_child(shooter)
+	add_child(shooter)
 
-    var line = 1
-    var count_in_line = 0
+	var line = 1
+	var count_in_line = 0
 
-    for i in 10:
-        var other = Piece.instantiate()
-        other.init((window.size / 2) + Vector2i(line * 10, 100 - 10 * i))
+	for i in 10:
+		var other = Piece.instantiate()
+		other.init((window.size / 2) + Vector2i(count_in_line * 50, 100 - 10 * line))
 
-        if count_in_line == line:
-            line += 1
-            count_in_line = 0
-        else:
-            count_in_line += 1
+		if count_in_line == line:
+			line += 1
+			count_in_line = 0
+		else:
+			count_in_line += 1
 
-        add_child(other)
+		add_child(other)
 
-    $Board.position = (window.size / 2) - Vector2i(350, 350)
+	$Board.position = (window.size / 2) - Vector2i(350, 350)
