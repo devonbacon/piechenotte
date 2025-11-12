3b9e38b70562109aa380d4115153fcef6650ed0f
Devon <d.coleman654@gmail.com>
2025-11-11 22:17:51 -0500

    temp

--- piechenotte/game.gd
+++ piechenotte/game.gd
@@ -8,10 +8,21 @@ func _ready():
     var shooter = Piece.instantiate()
     shooter.init(window.size / 2)
 
-    var other = Piece.instantiate()
-    other.init((window.size / 2) + Vector2i(0, 100))
-
     add_child(shooter)
-    add_child(other)
+
+    var line = 1
+    var count_in_line = 0
+
+    for i in 10:
+        var other = Piece.instantiate()
+        other.init((window.size / 2) + Vector2i(line * 10, 100 - 10 * i))
+
+        if count_in_line == line:
+            line += 1
+            count_in_line = 0
+        else:
+            count_in_line += 1
+
+        add_child(other)
 
     $Board.position = (window.size / 2) - Vector2i(350, 350)
