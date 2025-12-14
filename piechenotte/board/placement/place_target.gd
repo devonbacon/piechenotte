13ce7f2a909356a4476f7ba52251c38146ede1bd
Devon <d.coleman654@gmail.com>
2025-12-13 22:00:13 -0500

    clean: better borders

--- piechenotte/board/placement/place_target.gd
+++ piechenotte/board/placement/place_target.gd
@@ -5,14 +5,12 @@ var end := .25
 
 func start():
     var tween = get_tree().create_tween()
-    tween.tween_property($Sprite2D, "modulate:a", end, time)
-    tween.set_ease(Tween.EASE_IN)
+    tween.tween_property($Sprite2D, "modulate:a", end, time).set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_IN)
     tween.tween_callback(finish)
 
 func finish():
     var tween = get_tree().create_tween()
-    tween.tween_property($Sprite2D, "modulate:a", 1, time)
-    tween.set_ease(Tween.EASE_OUT)
+    tween.tween_property($Sprite2D, "modulate:a", 1, time).set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_OUT)
     tween.tween_callback(start)
 
 func _ready():
